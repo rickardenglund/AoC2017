40cebf2452ba01a66240800c05e3d3325007c7c4
Rickard Englund <rickard.englund@gmail.com>
2017-12-10 11:38:11 +0100

    day 10 part2 done

--- src/day10/day10.kt
+++ src/day10/day10.kt
@@ -1,52 +1,42 @@
 package day10
 
-val realLengths = listOf(206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3)
-val testLengths = listOf(3, 4, 1, 5)
-
-
-val appendList = listOf(17, 31, 73, 47, 23)
+val salt = listOf(17, 31, 73, 47, 23)
 
 fun main(args: Array<String>) {
+    val lengths = "206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3".toAsciiList()
+    lengths.addAll(salt)
 
-//    println(listOf(64, 7, 255).toHex())
-    val list = listOf(65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22)
-    println(list.sparseToDense())
-
-//    hash(listOf(10,10,10,10), 10)
-//    hash(appendList, 256)
+    val sparseHash = sparseHash(lengths, 256)
+    println(sparseHash.sparseToDense())
 }
 
-fun hash(lengths: List<Int>, size: Int) {
+fun sparseHash(lengths: List<Int>, size: Int): List<Int> {
     val list = (0 until size).toMutableList()
     var i = 0
     var skipSize = 0
-    for (hashLength in lengths) {
-        println("skip: ${skipSize.pad(2)} i : ${i.pad(3)} Length: ${hashLength.pad(3)} | ${list.print()}")
-        val reversed = list.sublistWrapping(i, (i+hashLength)%list.size, hashLength).asReversed()
+    for (round in 1..64) {
+        for (hashLength in lengths) {
+            val reversed = list.sublistWrapping(i, (i+hashLength)%list.size, hashLength).asReversed()
 
-        if (reversed.size != hashLength) {
-            println("Failed sublist: $reversed")
-            return
-        }
-//        println("Reversed: $reversed")
-        var iCopy = i
-        for (x in reversed) {
-            list[iCopy] = x
-            iCopy = (iCopy + 1)%list.size
-        }
+            if (reversed.size != hashLength) {
+                throw Exception("Failed sublist: $reversed")
+            }
+            var iCopy = i
+            for (x in reversed) {
+                list[iCopy] = x
+                iCopy = (iCopy + 1)%list.size
+            }
 
-        i = (i + skipSize + hashLength)%list.size
-        skipSize++
+            i = (i + skipSize + hashLength)%list.size
+            skipSize++
+        }
     }
-
-    println(list)
-    println("Result: ${list[0]} * ${list[1]} = ${list[0]*list[1]}")
+    return list
 }
 
 fun Int.pad(width: Int):String {
     return this.toString().padEnd(width, ' ')
 }
-
 
 fun List<Int>.sparseToDense(): String {
     if (this.size != 256) throw Exception("Invalid sparse hash size: ${this.size}")
@@ -69,7 +59,6 @@ fun List<Int>.xor(): Int {
 }
 
 fun <T>List<T>.sublistWrapping(start:  Int, stop:Int, length: Int): List<T> {
-//    println("$start $stop $length")
     if (stop <= start && length > 0) {
         val longList = this.toMutableList()
         longList.addAll(this.subList(0, stop))
@@ -77,6 +66,10 @@ fun <T>List<T>.sublistWrapping(start:  Int, stop:Int, length: Int): List<T> {
     }
 
     return this.subList(start, stop).toMutableList()
+}
+
+fun String.toAsciiList() : MutableList<Int> {
+    return this.map { it.toInt()}.toMutableList()
 }
 
 fun <T>List<T>.print():String {
