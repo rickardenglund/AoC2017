01a69210b3ee9090b2b50cfed4b50df3c6318ae9
Rickard Englund <rickard.englund@gmail.com>
2017-12-16 18:37:20 +0100

    day 16 use lambdas instead of classes for actions

--- src/day16/day16.kt
+++ src/day16/day16.kt
@@ -7,13 +7,10 @@ val nPrograms = 16
 val loopSize = 44
 val iterations = 1_000_000_000 % loopSize
 
-enum class ActionType {Spin, Exchange, Partner, NoAction}
-data class Action(val type: ActionType, val n1: Int, val n2: Int)
-
 fun main(args: Array<String>) {
     val programs = MutableList(nPrograms, { it -> it})
-    val moveList = input.split(",").map { createAction(it) }
-    val moves = Array(moveList.size, {i -> moveList[i]})
+    val moveList = input.split(",").map { createAction(it, programs) }
+    val moves = List(moveList.size, {i -> moveList[i]})
 
     val total = measureTimeMillis {
         dance(programs, moves)
@@ -21,16 +18,9 @@ fun main(args: Array<String>) {
     println("Total time: ${total/1000.toFloat()}")
 }
 
-fun dance(programs: MutableList<Int>, moves: Array<Action>) {
+fun dance(programs: MutableList<Int>, moves: List<() -> Unit>) {
     (1..iterations).forEach {
-        moves.forEach { action ->
-            when (action.type) {
-                ActionType.Spin -> programs.spin(action.n1)
-                ActionType.Exchange -> programs.exchange(action.n1, action.n2)
-                ActionType.Partner -> programs.partner(action.n1, action.n2)
-                ActionType.NoAction -> println("Warning no Action")
-            }
-        }
+        moves.forEach { it() }
     }
     programs.print()
 }
@@ -42,24 +32,24 @@ fun List<Int>.print() {
     println()
 }
 
-fun createAction(cmd: String): Action {
+fun createAction(cmd: String, programs: MutableList<Int>): () -> Unit {
     if (cmd.startsWith("s")) {
         val n = cmd.substring(1).toInt()
-        return Action(ActionType.Spin, n, -1)
+        return {programs.spin(n)}
     }
     if (cmd.startsWith("x")) {
         val indices = cmd.substring(1).split("/").map { it.toInt() }
         val i1 = indices[0]
         val i2 = indices[1]
-        return Action(ActionType.Exchange,i1,i2)
+        return {programs.exchange(i1, i2)}
     }
     if (cmd.startsWith("p")) {
         val names = cmd.substring(1).split("/")
         val id1 = programNames.indexOf(names[0])
         val id2 = programNames.indexOf(names[1])
-        return Action(ActionType.Partner,id1, id2)
+        return {programs.partner(id1, id2)}
     }
-    return Action(ActionType.NoAction, -1,-1)
+    return {}
 }
 
 fun MutableList<Int>.spin(n: Int) {
